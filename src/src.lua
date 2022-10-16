48a84d0caa6d3e477223fb83246abbf5eafb1b6a
FlowzyFC <115906808+FlowzyFC@users.noreply.github.com>
2022-10-15 21:46:30 -0400

    Update src.lua

--- src/src.lua
+++ src/src.lua
@@ -896,8 +896,8 @@ function Library:create(options)
 
 	rawset(mt, "creditsContainer", creditsTab.container)
 
-	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
-	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
+	creditsTab:credit{Name = "Flowzy", Description = "UI Library Developer & Script Dev", Discord = "Flowzy#3142"}
+	creditsTab:credit{Name = "Deity", Description = "UI Library Developer & Script Dev", Discord = "Requiem#2018"}
 
 	return mt
 end
